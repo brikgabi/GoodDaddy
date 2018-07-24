db29e814a5a8b271e06fe7df381ee846ee2ec188
asilbaugh <asilbaugh@godaddy.com>
2018-07-24 13:38:35 -0700

    fixes
    love u gabi

--- Home.py
+++ Home.py
@@ -17,7 +17,7 @@ def signUp():
     _name = request.form['inputName']
     _email = request.form['inputEmail']
     _password = request.form['inputePassword']
-    if _name and _email_ and _password:
+    if _name and _email and _password:
         return json.dumps({'html':'<span>All fields good !!</span>'})
     else:
         return json.dumps({'html':'<span>Enter the required fields</span>'})
